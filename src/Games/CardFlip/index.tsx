b7c56cd07b3ba24b1a2f1bb514d436f6bb7f11a6
Yu-Sen Hsu <yusenhsu@gmail.com>
2021-07-06 12:58:15 +0800

    reset isGameSet when isPlaying becomes false

--- src/Games/CardFlip/index.tsx
+++ src/Games/CardFlip/index.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as CardFlipStyle from './style';
 import Playground from './components/Playground';
 import Sidebar from './components/Sidebar';
 
-
 export default function CardFlip(): JSX.Element {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [isGameSet, setIsGameSet] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isPlaying && isGameSet) setIsGameSet(false);
+  }, [isPlaying, isGameSet]);
 
   return (
     <CardFlipStyle.Main>
